d05c1da4505aa0ca1d5da906eb9a36b6ffd8ba3f
root <cassinewman21@gmail.com>
2023-05-25 12:51:17 -0700

    6 8

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,5 +13,4 @@ int main(void)
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %zu bytes(s)\n", sizeof(long long int));
 	printf("size of a float %zu bytes(s)\n", sizeof(float));
-	return (0);
-}	
+	return (0);}	
