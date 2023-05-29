081ac3ea96ffa6cbb4e67ee09322d72e5a785cf7
root <cassinewman21@gmail.com>
2023-05-29 12:52:27 -0700

    5 1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	printf("%d", c);
+	printf("\n");
+	return (0);
+}
