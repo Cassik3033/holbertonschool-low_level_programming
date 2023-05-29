258666ab269edb0f81845a126b58f5f9ffdd74dd
root <cassinewman21@gmail.com>
2023-05-29 12:24:09 -0700

    3 1

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main- prints the alphabet
+ * main- prints the alphabet in lowercase and uppercase
  * Return: Always (Success)
  */
 
@@ -11,6 +13,9 @@ int main(void)
 	char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
+	putchar(c);
+
+	for (c = 'A' ; c <= 'Z' ; c++)
 	putchar (c);
 	putchar('\n');
 	return (0);
