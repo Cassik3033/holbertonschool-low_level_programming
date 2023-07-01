26be563f988e13422361b58efc95caf9776180a8
root <cassinewman21@gmail.com>
2023-07-01 11:54:34 -0700

    2-1

--- recursion/2-main.c
+++ recursion/2-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
- * return: always 0
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int main(void)
-{
-	int n;
-	n = _strlen_recursion("Corbin Coleman");
-	printf("%d\n", n);
-	return (0);
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
 }
