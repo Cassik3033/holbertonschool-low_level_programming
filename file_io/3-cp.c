4c052d45a1c34ae9802ce09def1435eb3d4c4a60
root <cassinewman21@gmail.com>
2023-07-17 21:08:10 -0700

    3-2

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,10 +31,8 @@ char *create_buffer(char *file)
  */
 void close_file(int fd)
 {
-	int c;
-	
-	c = close(fd);
-
+int c;
+c = close(fd);
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
@@ -52,7 +50,7 @@ void close_file(int fd)
  * if file_to cannot be created or written to - exit code 99
  * if file_to or file_from cannot be closed - exit code 100
  */
-int main(int argc, cchar *argv[])
+int main(int argc, char * argv[])
 {
 	int from, to, r, w;
 	char *buffer;
@@ -71,7 +69,7 @@ int main(int argc, cchar *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv [1]);
+					"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -79,7 +77,7 @@ int main(int argc, cchar *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Can't write to %s\n", argv [2]);
+					"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
